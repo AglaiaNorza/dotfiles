3b60f86fa9105194c98b9df0104ad2914a61d918
AglaiaNorza <thisisaglaia@gmail.com>
2024-07-12 12:57:35 +0200

    betterescape rewrite

--- .config/nvim/lua/plugins/better-escape.lua
+++ .config/nvim/lua/plugins/better-escape.lua
@@ -2,10 +2,12 @@ return {
 	"max397574/better-escape.nvim",
 	config = function()
 		require("better_escape").setup({
-			mapping = { "jk" },
-			timeout = vim.o.timeoutlen,
-			clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-			keys = "<Esc>",
+            timeout = vim.o.timeoutlen,
+            mappings = {
+                j = {
+                    j = '<Esc>'
+                },
+            }
 		})
 	end,
 }
