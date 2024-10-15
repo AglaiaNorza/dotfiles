80c800a34b0950d319310796b88bd21076520bdb
AglaiaNorza <thisisaglaia@gmail.com>
2024-10-15 23:27:16 +0200

    battery notif

--- .config/i3blocks/scripts/batterynoemoji.sh
+++ .config/i3blocks/scripts/batterynoemoji.sh
@@ -8,6 +8,8 @@ percentage=$(echo "$input" | awk -F", " '{print $2}' | awk -F"%" '{print $1}')
 # for PC
 if (( $percentage == 0 )); then
     exit 1
+elif (( $percentage <= 10 )); then
+    notify-send "battery low" "PLEASE CHARGE\!\!\!" --urgency=critical
 fi
 
 echo "$percentage%"
