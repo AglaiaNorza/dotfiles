02d3e36e93e751098f4ebcd1391653201fc0a195
AglaiaNorza <thisisaglaia@gmail.com>
2024-10-17 14:46:33 +0200

    keyboard disable script

--- .config/i3blocks/scripts/keyboard.sh
+++ .config/i3blocks/scripts/keyboard.sh
@@ -4,3 +4,11 @@
 # (i should probably look for the string following "layout" but since i know the layout is going to be in the last two characters (+\n), i can do this instead)
 
 echo $(setxkbmap -query | tail -c 3)
+
+if (( $BLOCK_BUTTON == 1 )); then
+    xinput enable 12
+    notify-send -u normal "keyboard enabled"
+elif (( $BLOCK_BUTTON == 3 )); then
+    xinput disable 12
+    notify-send -u critical "keyboard disabled"
+fi
