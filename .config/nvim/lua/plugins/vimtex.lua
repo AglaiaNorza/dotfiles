70121714913f1ad897c271fe920804c220ef87cf
AglaiaNorza <thisisaglaia@gmail.com>
2025-11-09 13:22:56 +0100

    tex line

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,11 +1,17 @@
 return {
   "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
+  lazy = false, 
+  -- tag = "v2.15", 
   init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
-  end,
+        vim.g.vimtex_view_method = "zathura"
+
+        --vim.g.vimtex_view_general_viewer = 'okular'
+        --vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
+
+
+        vim.g.vimtex_quickfix_open_on_warning = 0
+        vim.g.vimtex_quickfix_open_on_error = 1
+    end,
     config = function()
         vim.cmd("filetype plugin indent on")
         vim.cmd("syntax enable")
